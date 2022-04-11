e8ea2533d6d64add86c89d91af2a5b75d08e9dfd
Soyeb <tssoyeb@gmail.com>
2022-04-11 10:48:12 +0600

    facebook auth provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import app from './firebase.init';
-import {getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth'
+import {FacebookAuthProvider, getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth'
 import { useState } from 'react';
 
 const auth=getAuth(app)
@@ -10,6 +10,7 @@ function App() {
   const [user,setUser]=useState({})
   const gooleProvider =new GoogleAuthProvider();
   const githubProvider =new GithubAuthProvider();
+  const facebookProvider = new FacebookAuthProvider()
  
 
   const handleGoogleSignin=()=>{
@@ -35,6 +36,13 @@ function App() {
      .catch(error =>{
        console.log(error);
      })
+  }
+
+  const handleFacebookSignIn =()=>{
+signInWithPopup(auth,facebookProvider)
+.then(result=>{
+  const user = result.user
+})
   }
   // const handleGoogleSignOut=()=>{
   //   signOut(auth)
@@ -65,7 +73,7 @@ function App() {
        !user.displayName? <> 
        <button onClick={handleGoogleSignin}>Google SignIn</button>
        <button onClick={handleGithubSignIn}>Github SignIn</button>
-       <button onClick={handleGithubSignIn}>Facebook SignIn</button>
+       <button onClick={handleFacebookSignIn}>Facebook SignIn</button>
        </>
        :
        <>
